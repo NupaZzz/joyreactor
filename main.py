357b7199d1fa8479a097b5db99db6fb6f6c5f59d
NupaZzz <Nupalolz@gmail.com>
2023-09-19 00:11:16 +0300

    updage telegram bot function

--- main.py
+++ main.py
@@ -1,85 +1,96 @@
-import requests
-from bs4 import BeautifulSoup
 from aiogram import Bot, types, Dispatcher, F
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command, CommandObject
 from aiogram.types import Message
-import asyncio
-from config import variables
+import requests
+from bs4 import BeautifulSoup
 import json
+import asyncio
+import logging
+from variables import variables
+from modules import user_configs
 
+dp=Dispatcher()
+visited_links = set()
+configs = user_configs.load_configs()
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
 }
 
-def save_configs(user_configs):
-    with open('user_configs.json', 'w') as f:
-        json.dump(user_configs, f, indent=4)
-
-def load_configs():
-    try:
-        with open('user_configs.json', 'r') as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return {}
-    
-user_configs = load_configs()
-dp=Dispatcher()
-
 def get_url():
     response = requests.get(url=variables.url, headers=headers)
     soup = BeautifulSoup(response.text, "html.parser")
-    with open('page_data.json', 'w', encoding="utf-8") as file:
+    with open(f'page_data.json', 'w', encoding="utf-8") as file:
         file.write(str(soup))
 
-def get_links():
-    with open('page_data.json', 'r', encoding='utf-8') as file:
+def get_links(user_id):
+    with open(f'page_data.json', 'r', encoding='utf-8') as file:
         data = file.read()
     soup = BeautifulSoup(data, "html.parser")
     href_list = []
     for link_wr in soup.find_all(class_='link_wr'):
         for link in link_wr.find_all('a'):
             href = link.get('href')
-            if href not in variables.visited_links:
-                variables.visited_links.add(href)
+            if href not in visited_links:
+                visited_links.add(href)
                 href_list.append(href)
-    return href_list
+    
+    # Загрузка уже сохраненных ссылок
+    try:
+        with open(f'{user_id}_links.json', 'r', encoding='utf-8') as file:
+            saved_links = json.load(file)
+    except FileNotFoundError:
+        saved_links = []
+
+    # Добавление новых ссылок, если они еще не сохранены
+    new_links = [link for link in href_list if link not in saved_links]
+    saved_links.extend(new_links)
+
+    # Сохранение обновленного списка ссылок
+    with open(f'{user_id}_links.json', 'w', encoding='utf-8') as file:
+        json.dump(saved_links, file)
+
+    return new_links
 
 @dp.message(Command('joy_url'))
 async def joy_url_start(message: types.Message):
     user_id = str(message.from_user.id)
     url_type = message.text.split()[1]
-    if not url_type:
-        await message.answer("Некорректная команда. Укажите URL после /joy_url.")
-        return
     if url_type == 'default':
-        user_configs[user_id] = variables.url
+        configs[user_id] = variables.url
     elif url_type == 'm':
-        user_configs[user_id] = variables.url_m
+        configs[user_id] = variables.url_m
     elif url_type == 'old':
-        user_configs[user_id] = variables.url_old
+        configs[user_id] = variables.url_old
     else:
         await message.answer("Некорректный URL")
         return
-    save_configs(user_configs)
-    await message.answer(f"Установлен URL: {user_configs[user_id]}")
+    user_configs.save_configs(configs)
+    await message.answer(f"Установлен URL: {configs[user_id]}")
 
 @dp.message(Command('joy'))
 async def joy_start(message: types.Message):
     user_id = str(message.from_user.id)
-    while True:
+    if user_id not in configs:
+        await message.answer("Пожалуйста, выберите URL с помощью комманды /joy_url со следующими из значений: m, old, default. Пример: /joy_url m")
+        return
+    running = True
+    while running:
         get_url()
-        links = get_links()
+        links = get_links(user_id)
         if links:
             for link in links:
-                await message.answer(f"{user_configs[user_id]}{link}")
-                await asyncio.sleep(10)
-            await asyncio.sleep(10)
+                await message.answer(f"{configs[user_id]}{link}")
+                await asyncio.sleep(5)
+        await asyncio.sleep(10)
+        if not running:
+            break
 
 async def main():
     bot = Bot(variables.TOKEN, parse_mode=ParseMode.HTML)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
